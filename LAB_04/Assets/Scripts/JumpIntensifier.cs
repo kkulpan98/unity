9edb2393b4aa84f86a93dc0e54e6ef5064d21c86
kamkul <kkulpan98@gmail.com>
2020-11-02 11:14:34 +0100

    zrobiono lab 04

---
+++ LAB_04/Assets/Scripts/JumpIntensifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpIntensifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.gameObject.tag == "JumpIntensifier")
+            Debug.Log("Wyzej skacz");
+    }
+}
---
+++ LAB_04/Assets/Scripts/Zadanie1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Zadanie1 : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    public int objectsNumber = 10;
+    int objectCounter = 0;
+    // obiekt do generowania
+    public GameObject block;
+    public Material[] materialy = new Material[5];
+
+    void Start()
+    {
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<int> pozycje_x = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(10));
+        List<int> pozycje_z = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(10));
+
+        for (int i = 0; i < objectsNumber; i++)
+        {
+            this.positions.Add(new Vector3(pozycje_x[i], 5, pozycje_z[i]));
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywołano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            GameObject tmpObject = Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            tmpObject.GetComponent<MeshRenderer>().material = materialy[UnityEngine.Random.Range(0,4)];
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
